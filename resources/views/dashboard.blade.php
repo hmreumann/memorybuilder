fb6fc4c42529a39b738fab50cd5cdfc5b0ba1044
Hernan Martinez Reumann <hmreumann@hotmail.com>
2021-06-20 22:24:39 -0300

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,32 @@
         <div class="w-full md:w-64 p-6 bg-blue-200 border-r  border-gray-200">
             @livewire('exams')
         </div>
-        
+        <div class=" m-6 space-y-6 flex-col">
+            @forelse(auth()->user()->exams as $exam)
+            <div class="bg-white rounded p-6 shadow">
+                <div>
+                    {{$exam->title}}
+                </div>
+                @if($exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first() !== null)
+                <a href="{{route('tests.show',$exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first())}}">
+                    <div>
+                        Tested {{$exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first()->updated_at->diffForHumans()}}<br>
+                        {{$exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first()->results_correct / $exam->questions->count() * 100 }} % Completed
+                    </div>
+                </a>
+
+                @else
+                <div>
+                    Not Yet Tested<br>
+                    <a href="{{route('tests.create',$exam)}}">Test now</a>
+                </div>
+                @endif
+            </div>
+            @empty
+            <div>
+                Your activity will be shown here.
+            </div>
+            @endforelse
+        </div>
     </div>
 </x-app-layout>
