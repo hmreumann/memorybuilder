44e2064de5692818adb4644bab6ff6480af9cf80
Hernan Martinez Reumann <hmreumann@hotmail.com>
2021-06-21 12:46:51 -0300

    Added sharedexams to dashboard with a grid

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,26 +5,26 @@
         </h2>
     </x-slot>
 
-    <div class="md:flex md:h-screen">
+    <div class="md:flex ">
         <div class="w-full md:w-64 p-6 bg-blue-200 border-r  border-gray-200">
             @livewire('exams')
         </div>
-        <div class=" m-6 space-y-6 flex-col">
-            @forelse(auth()->user()->exams as $exam)
+        <div class="grid grid-cols-1 sm:grid-cols-3 gap-6 p-6 place-content-start">
+            @forelse(auth()->user()->exams->merge(auth()->user()->sharedExams) as $exam)
             <div class="bg-white rounded p-6 shadow">
-                <div>
+                <div class="font-bold">
                     {{$exam->title}}
                 </div>
                 @if($exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first() !== null)
                 <a href="{{route('tests.show',$exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first())}}">
-                    <div>
+                    <div class="text-green-500 hover:text-green-700">
                         Tested {{$exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first()->updated_at->diffForHumans()}}<br>
-                        {{$exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first()->correct_answers / $exam->questions->count() * 100 }} % Completed
+                        {{round($exam->tests()->where('user_id',auth()->user()->id)->orderByDesc('updated_at')->first()->correct_answers / $exam->questions->count() * 100) }} % Completed
                     </div>
                 </a>
 
                 @else
-                <div>
+                <div class="text-green-500 hover:text-green-700">
                     Not Yet Tested<br>
                     <a href="{{route('tests.create',$exam)}}">Test now</a>
                 </div>
