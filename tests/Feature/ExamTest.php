332ba36d164eb2eaf9ebba1dc640a9e69ab42baa
Hernan Martinez Reumann <hmreumann@hotmail.com>
2021-08-05 16:10:05 -0300

    Solved error in tests

--- tests/Feature/ExamTest.php
+++ tests/Feature/ExamTest.php
@@ -37,43 +37,4 @@ class ExamTest extends TestCase
         $this->assertCount(0, Exam::all());
         $this->assertCount(0, Question::all());
     }
-
-    /** @test */
-    public function only_the_owner_of_the_exam_can_delete_the_user ()
-    {
-        $user = User::factory()->create();
-        $exam = Exam::factory()->create();
-
-        $this->assertCount(1, Exam::all());
-        $second_user = User::factory()->create();
-        $response = $this->actingAs($second_user)->delete('/exams/1')->assertStatus(403);
-
-        $this->assertCount(1, Exam::all());
-    }
-
-    /** @test */
-    public function the_delete_link_is_being_rendered ()
-    {
-        $user = User::factory()->create();
-        $exam = Exam::factory()->create();
-
-        $response = $this->actingAs($user)->get('/dashboard')->assertSee('<form action="http://memorybuilder.devel/exams/1" method="POST">', false);
-
-        $this->assertCount(1, Exam::all());
-    }
-
-    /** @test */
-    public function the_delete_link_is_not_being_rendered_if_it_is_another_user ()
-    {
-        $user = User::factory()->create();
-        $exam = Exam::factory()->create();
-
-        $second_user = User::factory()->create();
-
-        $second_user->sharedExams()->attach($exam->id);
-
-        $response = $this->actingAs($second_user)->get('/dashboard')->assertDontSee('<form action="http://memorybuilder.devel/exams/1" method="POST">', false);
-
-        $this->assertCount(1, Exam::all());
-    }
 }
